6cda05205e70f095a0bfbc9c2772ef17b37d4a1a
John Lim <jolim24601@gmail.com>
2016-02-15 09:35:36 -0500

    Add google maps with a map marker to my location. Change avatar photo and add max-height of 250px. Add third project to portfolio.

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,26 +39,19 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(40.8516111, -73.9949477));
 });
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(40.8516111, -73.9949477),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
             "elementType": "geometry",
@@ -169,16 +162,11 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
     map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var image = 'images/map-marker.png';
+    var myLatLng = new google.maps.LatLng(40.8516111, -73.9949477);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
